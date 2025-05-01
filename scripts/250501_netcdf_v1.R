cfd26c45c377b879735d04015e4fe3aabbf879c8
Peter Regier <peter.regier@pnnl.gov>
2025-05-01 17:53:55 -0600

    upload og netcdf

---
+++ scripts/250501_netcdf_v1.R
@@ -0,0 +1,11 @@
+## FIgure out what's in the 
+
+# QC flags
+# 0 = good
+# 1 = missing values
+# 2 = too low
+# 4 = too high
+# 8 = spike detection/non-real data
+
+## Access Key ID: AKIATW2JYZRGMAILEAME
+## Secrete Key: PwBtCRdEb/qtxAT9h/YlRGkTebAnBODbDtDKvaEdd
